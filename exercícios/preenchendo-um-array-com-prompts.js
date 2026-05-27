bffadd32d739475774985a7251772394da1a9ab4
Gabriel <gabriel.braga0125@gmail.com>
2026-05-26 21:03:16 -0300

    Update preenchendo-um-array-com-prompts.js

--- exercícios/preenchendo-um-array-com-prompts.js
+++ exercícios/preenchendo-um-array-com-prompts.js
@@ -1,15 +1,15 @@
 var lista = []
-var regADD = prompt("Quantos registros deseja adicionar? ")
+var regADD = prompt("Quantos jogos deseja adicionar à sua lista de desejos? ")
 
 if (regADD >=1){
 
 for(i=1; i<=regADD; i++){
+    alert("\nAdicionando o "+i+"º"+ " jogo")
     
-    var reg = prompt("Qual será o registro " + i +"? ")
-
+    var reg = prompt("Digite o nome do jogo: ")
     lista.push(reg)
 }
-alert("Registros: " + lista)
+alert("\nJogos registrados: " + lista)
 }
 else{
     alert("Número de registros inválido")
